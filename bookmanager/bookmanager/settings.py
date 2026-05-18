9a5887f66b3e7d7768a5f8a5e5673ae3815312ed
ccy <2074441723@qq.com>
2026-05-18 16:30:50 -0400

    Update bookmanager

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/ooooo/'
+STATIC_URL = '/static/'
 
 # Tell the system where our images are located
 STATICFILES_DIRS = [
